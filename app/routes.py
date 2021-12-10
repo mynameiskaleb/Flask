83835f7045b8e6c24743f99dbf8121f49a9089c9
KalebKeith <kalebkeith@pm.me>
2021-12-10 00:30:35 -0500

    Made some changes to get a working app.

--- app/routes.py
+++ app/routes.py
@@ -2,28 +2,39 @@ from app import app
 from flask import render_template, flash, url_for
 from app.forms import LoginForm, SignupForm
 
+#web app homepage
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
+#user's home page
 @app.route('/home')
-@login_required
 def home():
+    # get user's subscribed stocks
+    # get user's buy/sell history
     return render_template('home.html')
 
-@app.route('/login')
+#login user
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Logging in...')
-        return redirect(url_for('home')) #redirects to user's home page
+        return redirect(url_for('home'))
     return render_template('login.html', form=form)
 
-@app.route('/signup')
+#signup user
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
         flash('Creating account...')
-        return redirect(url_for('home'))
+        return redirect(url_for('email_verification'))
     return render_template('signup.html', form=form)
+
+#new user email verification
+@app.route('/emailverification')
+def email_verification():
+    #placeholder for email email_verification
+    return redirect(url_for('home'))
